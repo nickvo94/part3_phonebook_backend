4121a53483998284eda4e16c0bd91c175fe258f0
perseus0832 <nickvo2308@gmail.com>
2020-09-04 12:44:42 +0300

    uibuild

---
+++ build/precache-manifest.f57f02fddb38f6cb09bce972dd3146df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3f55229a31c3f6afadf7c8a31ed2c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28366504ff339d086cec",
+    "url": "/static/css/main.5e32e95f.chunk.css"
+  },
+  {
+    "revision": "2469ec848eef6bb55628",
+    "url": "/static/js/2.90705a08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90705a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28366504ff339d086cec",
+    "url": "/static/js/main.acea077b.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
